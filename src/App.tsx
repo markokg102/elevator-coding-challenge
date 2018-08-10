296a55f7c90a56fe9aeeed6035c82f2907ac1943
Marko Manojlovic <markokg102@gmail.com>
2018-08-10 12:40:22 +0200

    create react app with typescript support, initial building class

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import './App.css';
+
+import Building from './features/model/Building'
+
+class App extends React.Component {
+
+  private building: Building;
+
+  constructor(props: any) {
+    super(props);
+
+    this.building = new Building(20, 2);
+
+  }
+
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Elevator Coding Challenge</h1>
+          Building with: {this.building.ammountOfFloors} floors and {this.building.ammountOfElevators} elevators.
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/features/model/Building.tsx
@@ -0,0 +1,13 @@
+class Building {
+
+    public ammountOfFloors: number;
+    public ammountOfElevators: number;
+
+    constructor(ammountOfFloorsParam: number, ammountOfElevatorsParam: number) {
+        this.ammountOfFloors = ammountOfFloorsParam;
+        this.ammountOfElevators = ammountOfElevatorsParam;
+    }
+
+}
+
+export default Building;
