10ea42f9cd7ea83a53a5807f6e2edd28b1df02d2
Marko Manojlovic <markokg102@gmail.com>
2018-08-10 13:07:24 +0200

    request que for building

--- src/features/model/Building.tsx
+++ src/features/model/Building.tsx
@@ -1,11 +1,24 @@
+import Request from './Request';
+
 class Building {
 
     public ammountOfFloors: number;
     public ammountOfElevators: number;
 
+    private queueOfRequests: Request[];
+
     constructor(ammountOfFloorsParam: number, ammountOfElevatorsParam: number) {
         this.ammountOfFloors = ammountOfFloorsParam;
         this.ammountOfElevators = ammountOfElevatorsParam;
+        this.queueOfRequests = [];
+    }
+
+    public newRequest(request: Request) {
+        this.queueOfRequests.push(request);
+    }
+
+    public removeOldestDoneRequestAtBegining() {
+        this.queueOfRequests.shift();
     }
 
 }
---
+++ src/features/model/Request.tsx
@@ -0,0 +1,18 @@
+export enum RequestType {
+    RequestForElevator = 1,
+    RequestDestination,
+}
+
+class Request {
+
+    public requestedFloor: number;
+    public requestType: RequestType;
+
+    constructor(requestedFloorParam: number, requestTypeParam: RequestType) {
+        this.requestedFloor = requestedFloorParam;
+        this.requestType = requestTypeParam;
+    }
+
+}
+
+export default Request;
